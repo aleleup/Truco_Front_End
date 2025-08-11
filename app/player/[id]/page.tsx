fca4f55175d86507204449b6330aa0dcf2e9a20f
Alejandro Leupold de Souza <aleupold@dappsfactory.io>
2025-08-11 19:30:24 -0300

    fst commit 2.0

--- app/player/[id]/page.tsx
+++ app/player/[id]/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export const PlayerPlayGround = () => {
+interface PlayerPlayGroundProps {
+    id: String
+}
+/**
+ * 
+@todo check in the server if player_id has already been taken
+* @returns 
+ */
+ function PlayerPlayGround({id}: PlayerPlayGroundProps){
   return (
     <div>PlayerPlayGround</div>
   )
 }
+
+
+export default PlayerPlayGround
